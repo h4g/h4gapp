ac44006ae6702511cc3ad0ec6b04a1afd1474fb5
namelivia <ohcan2@gmail.com>
2014-04-04 14:53:20 +0200

    Cambiando algunas cosas

--- js/load_map.js
+++ js/load_map.js
@@ -10,8 +10,34 @@ function getFeatures() {
         return reader.read(features);
 }
 
+function locateMe(){
+    console.log("Localizando....");
+    navigator.geolocation.getCurrentPosition(function(position) {       
+	    var lonLat = new OpenLayers.LonLat(position.coords.longitude,
+				    position.coords.latitude)
+		      .transform(
+				  new OpenLayers.Projection("EPSG:4326"), //transform from WGS 1984
+					      map.getProjectionObject() //to Spherical Mercator Projection
+					    );
+	    var point = new OpenLayers.Geometry.Point(lonLat.lon, lonLat.lat);
+	    var userLayer = new OpenLayers.Layer.Vector("User", {
+		styleMap: new OpenLayers.StyleMap({
+		    externalGraphic: "img/skater-green.png",
+		    graphicOpacity: 1.0,
+		    graphicWidth: 50,
+		    graphicHeight: 50/*,
+		    graphicYOffset: -26*/
+		})
+	    });
+	    userLayer.addFeatures([new OpenLayers.Feature.Vector(point)]);
+	    map.addLayer(userLayer);
+	    map.setCenter(lonLat, 18 // Zoom level
+	    );
+	});
+}
+
 //Loads the map
-function LoadMap(){
+function LoadMap(whatDiv,locate){
     var skatersLayer = new OpenLayers.Layer.Vector("Skaters", {
         styleMap: new OpenLayers.StyleMap({
             externalGraphic: "img/skater-red.png",
@@ -21,19 +47,10 @@ function LoadMap(){
             graphicYOffset: -26*/
         })
     });
-    var userLayer = new OpenLayers.Layer.Vector("User", {
-        styleMap: new OpenLayers.StyleMap({
-            externalGraphic: "img/skater-green.png",
-            graphicOpacity: 1.0,
-            graphicWidth: 50,
-            graphicHeight: 50/*,
-            graphicYOffset: -26*/
-        })
-    });
         var skaters = getFeatures();
         skatersLayer.addFeatures(skaters);
 	map = new OpenLayers.Map({
-	div: "map",
+	div: whatDiv,
         theme: null,
         controls: [
             new OpenLayers.Control.Attribution(),
@@ -45,27 +62,16 @@ function LoadMap(){
             new OpenLayers.Control.Zoom()
         ],
         layers: [
-            new OpenLayers.Layer.Stamen("toner", null, {
+            new OpenLayers.Layer.OSM(null, null, {
                 transitionEffect: 'resize'
             }),
-	    skatersLayer,
-	    userLayer
+	    skatersLayer
         ],
         center: new OpenLayers.LonLat(742000, 5861000),
         zoom: 3
     });
 
-
-    navigator.geolocation.getCurrentPosition(function(position) {       
-            var lonLat = new OpenLayers.LonLat(position.coords.longitude,
-                                    position.coords.latitude)
-                      .transform(
-                                  new OpenLayers.Projection("EPSG:4326"), //transform from WGS 1984
-                                              map.getProjectionObject() //to Spherical Mercator Projection
-                                            );
- 	    var point = new OpenLayers.Geometry.Point(lonLat.lon, lonLat.lat);
-	    userLayer.addFeatures([new OpenLayers.Feature.Vector(point)]);
-            map.setCenter(lonLat, 18 // Zoom level
-            );
-        });
+    if (locate){
+	locateMe();
+    }
 }
