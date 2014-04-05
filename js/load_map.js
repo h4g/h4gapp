dd1234e0b355da412d928aa3720fc9055a2fd57b
namelivia <ohcan2@gmail.com>
2014-04-05 15:07:04 +0200

    Estilos, version presentable

--- js/load_map.js
+++ js/load_map.js
@@ -8,7 +8,8 @@ function getFeatures(points) {
 					      map.getProjectionObject() //to Spherical Mercator Projection
 					    );
         	var template = { "type": "Feature", "geometry": {"type": "Point", "coordinates":
-[lonLat.lon,lonLat.lat]}};
+[lonLat.lon,lonLat.lat]},"properties":
+{"name":points[i].name,"description":points[i].description,"audiodata":points[i].audiodata}};
 		arrayFeatures.push(template);
 	}
         var features = {
@@ -22,17 +23,72 @@ function getFeatures(points) {
 function getAllSpots(points){
     var audioSpotsLayer = new OpenLayers.Layer.Vector("audioSpots", {
         styleMap: new OpenLayers.StyleMap({
-            externalGraphic: "img/checkpoint02.png",
+            externalGraphic: "img/checkpoint_audio.png",
             graphicOpacity: 1.0,
-            graphicWidth: 25,
-            graphicHeight: 64/*,
+            graphicWidth: 27,
+            graphicHeight: 130/*,
             graphicYOffset: -26*/
-        })
+        }),
+	eventListeners:{
+                'featureselected':function(evt){
+                    var feature = evt.feature;
+		    var centerPoint = new OpenLayers.LonLat(feature.geometry.x, feature.geometry.y);
+		    $('#whatName').html(feature.attributes.name);
+		    $('#whatDescription').html(feature.attributes.description);
+		    var audioElement = document.getElementById('mapPlayer');
+		    var blob = new Blob(feature.attributes.audiodata, { type: "audio/ogg" });
+		    //audioElement.src = window.URL.createObjectURL(blob);
+	    	    map.setCenter(centerPoint, 18);
+                },
+                'featureunselected':function(evt){
+                    var feature = evt.feature;
+                }
+	}	
     });
     var audioSpots = getFeatures(points);
-    console.log(audioSpots);
+
+    var audioHover = new OpenLayers.Control.SelectFeature(
+        audioSpotsLayer,
+	{
+	    hover: true,
+	    onSelect: hoverAudio,
+	    autoActivate: true
+	}
+    );
+
+    var audioHoverOut = new OpenLayers.Control.SelectFeature(
+        audioSpotsLayer,
+	{
+	    highlightOnly: true,
+	    hover: true,
+	    onUnselect: hoverAudioOut,
+	    autoActivate: true
+	}
+    );
+
+    var audioControl = new OpenLayers.Control.SelectFeature(
+        audioSpotsLayer,
+	{
+	    onSelect: clickAudio,
+	    autoActivate: true
+	}
+    );
+  
+    map.addControl(audioControl);
     audioSpotsLayer.addFeatures(audioSpots);
     map.addLayer(audioSpotsLayer);
+}
+
+function clickAudio(){
+    $('#popupBasic').show();
+}
+
+function hoverAudio(){
+    document.body.style.cursor = 'pointer';
+}
+
+function hoverAudioOut(){
+    document.body.style.cursor = 'auto';
 }
 
 function locateMe(){
@@ -49,10 +105,10 @@ function locateMe(){
 	    var point = new OpenLayers.Geometry.Point(lonLat.lon, lonLat.lat);
 	    var userLayer = new OpenLayers.Layer.Vector("User", {
 		styleMap: new OpenLayers.StyleMap({
-		    externalGraphic: "img/checkpoint02.png",
+		    externalGraphic: "img/checkpoint.png",
 		    graphicOpacity: 1.0,
-		    graphicWidth: 25,
-		    graphicHeight: 64/*,
+		    graphicWidth: 27,
+		    graphicHeight: 130/*,
 		    graphicYOffset: -26*/
 		})
 	    });
